923a398e56ddd047eabf4e09c1724cf6f48db6da
danil tsoy <abdullaevdanil2002@gmail.com>
2022-10-25 22:55:51 +0600

    #3 added class Hand. Added all needed methods in it: method to show, take card and change card

--- index.ts
+++ index.ts
@@ -33,8 +33,39 @@ class Deck {
     }
 }
 
+class Hand {
+    cardsInHand: Card[] = []
+    getCard(deck: Deck): void{
+        const newCard: Card = deck.giveCard()
+        this.cardsInHand.push(newCard)
+    }
+    showHand(): void {
+        if (this.cardsInHand.length > 0){
+            for (let i = 0; i < this.cardsInHand.length; i++){
+                let cardNumber: number = i + 1
+                console.log(`${cardNumber}. ${this.cardsInHand[i].suit} ${this.cardsInHand[i].rank}`)
+            }
+        } else {
+            console.log('Hand is empty')
+        }
+    }
+    changeCard(cardNum: number, deck: Deck): void {
+        this.cardsInHand.splice(cardNum - 1, 1)
+        this.getCard(deck)
+    }
+}
+
 const deck = new Deck()
-deck.showDeck()
 deck.mixDeck()
-deck.showDeck()
+
+const hand = new Hand()
+
+hand.getCard(deck)
+hand.getCard(deck)
+hand.getCard(deck)
+hand.getCard(deck)
+hand.getCard(deck)
+hand.showHand()
+
+
 
