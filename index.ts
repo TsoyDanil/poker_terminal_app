1387f63578108153023cb0d38156069db04711b5
danil tsoy <abdullaevdanil2002@gmail.com>
2022-10-25 19:55:01 +0600

    #1 Changed method of card creation/ Now it will get correct suit and rank

--- index.ts
+++ index.ts
@@ -1,16 +1,17 @@
-const rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 const suits = ["♠", "♥", "♦", "♣"]
 class Card {
     rank: string
     suit: string
-    constructor() {
-        this.rank = rank[Math.floor(Math.random()*rank.length)]
-        this.suit = suits[Math.floor(Math.random()*suits.length)]
+    constructor(rank: string, suit: string) {
+        this.rank = rank
+        this.suit = suit
     }
 }
 
-for (let i = 0; i < 52; i++){
-    const card = new Card()
-    console.log(card);
-    
-}
+// for (let i = 0; i < ranks.length; i++){
+//     for (let j = 0; j < suits.length; j++){
+//         const card = new Card(ranks[i], suits[j])
+//         console.log(card)
+//     }
+// }
