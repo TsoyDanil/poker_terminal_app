5ff5837cce57a6e5c48e22264aa7c4c17d4d52d2
danil tsoy <abdullaevdanil2002@gmail.com>
2022-10-25 19:46:15 +0600

    #1 Created class Card. with constractor build it with using arrays of cards info. Right now i give it randomly

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log(1);
+const rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+const suits = ["♠", "♥", "♦", "♣"]
+class Card {
+    rank: string
+    suit: string
+    constructor() {
+        this.rank = rank[Math.floor(Math.random()*rank.length)]
+        this.suit = suits[Math.floor(Math.random()*suits.length)]
+    }
+}
+
+for (let i = 0; i < 52; i++){
+    const card = new Card()
+    console.log(card);
+    
+}
